7302032bc50dd1565341715f6388bda15452f8a6
StarbuckZero <starbuckzero@gmail.com>
2020-01-04 15:42:59 -0500

    Added Color Support to Buttons

--- src/Main.hx
+++ src/Main.hx
@@ -1,8 +1,5 @@
 package;
 
-
-
-
 #if !html5
 import sys.FileSystem;
 #end
@@ -31,13 +28,12 @@ import openfl.Lib;
  * ...
  * @author Erick Feiling
  */
-class Main extends Sprite
-{
+class Main extends Sprite {
 	private static var BUTTON_FONT_SIZE:Int = 14;
 
 	private var _dataObj:Dynamic;
 
-	private var _tabArea:TabPane;
+	private var _tabArea:SoundTabPane;
 
 	private var _scrollPanel:ScrollPane;
 	private var _buttonHeight:Int;
@@ -50,192 +46,188 @@ class Main extends Sprite
 	private var _currentMusic:SoundChannel;
 	private var _currentSoundFX:SoundChannel;
 
+	private var defaultTextColor:Int = 0x000000;
+	private var defaultButtonColor:Int = 0xCCCCCC;
 
-	public function new() 
-	{
+
+	public function new() {
 		super();
-		
+
 		init();
-		
-		//TODO: If Icon is founded force to be 40x40
 
+		// TODO: If Icon is founded force to be 40x40
 
-		//TODO: If no icon use default
-
+		// TODO: If no icon use default
 
 		// Assets:
 		// openfl.Assets.getBitmapData("img/assetname.jpg");
 
-		addEventListener(Event.ADDED_TO_STAGE,onAddToStage,false,0,true);
-
+		addEventListener(Event.ADDED_TO_STAGE, onAddToStage, false, 0, true);
 	}
-	
-	private function onAddToStage(event:Event):Void
-	{
-		
+
+	private function onAddToStage(event:Event):Void {
 		stage.scaleMode = StageScaleMode.NO_SCALE;
 		stage.align = StageAlign.TOP_LEFT;
-		stage.addEventListener(Event.RESIZE,onResize,false,0,true);
+		stage.addEventListener(Event.RESIZE, onResize, false, 0, true);
 	}
 
-	private function onResize(event:Event):Void
-	{
+	private function onResize(event:Event):Void {
 		_tabArea.y = _tabArea.x = 0;
 
-		
 		_tabArea.width = Std.int(stage.stageWidth);
 		_tabArea.height = Std.int(stage.stageHeight);
 		_scrollPanel.height = Std.int(stage.stageHeight - _buttonHeight);
-		
 
 		// Upload scroll panel location
-		if(_currentButton != null && _scrollPanel.parent != null)
-		{
+		if (_currentButton != null && _scrollPanel.parent != null) {
 			_scrollPanel.x = _currentButton.x + _scrollPanel.width;
 			_scrollPanel.y = _buttonHeight + 6;
 		}
-		
-		if(_tabArea.selectedIndex >= 0)
-		{
+
+		if (_tabArea.selectedIndex >= 0) {
 			_tabArea.getSelectedItem().content.width = Std.int(_tabArea.width);
 			_tabArea.getSelectedItem().content.height = Std.int(_scrollPanel.height);
 
-			cast(_tabArea.getSelectedItem().content,VerticalContainer).draw();
+			cast(_tabArea.getSelectedItem().content, VerticalContainer).draw();
 		}
-			
 
 		_tabArea.draw();
 		_scrollPanel.draw();
 		_tabArea.scrollPane.draw();
-
 	}
 
-	private function init():Void
-	{
-		//Grab JSON Data file
-		if(Assets.exists("data/menu.json"))
-		{
+	private function init():Void {
+		// Grab JSON Data file
+		if (Assets.exists("data/menu.json")) {
 			trace(FileSystem.absolutePath(""));
 
 			_soundManager = new SoundManager();
-			
-			_dataObj = Json.parse(Assets.getText("data/menu.json"));
-			//trace(_dataObj);
 
-			//Start building out Tab Menu
-			if(Reflect.hasField(_dataObj,"SoundBoard"))
-			{
-				
-				
-				_tabArea = new TabPane();
-				var soundBoardList:Array<Dynamic> = Reflect.field(_dataObj,"SoundBoard");
+			_dataObj = Json.parse(Assets.getText("data/menu.json"));
+
+			// Start building out Tab Menu
+			if (Reflect.hasField(_dataObj, "SoundBoard")) {
+
+				_tabArea = new SoundTabPane();
+				var soundBoardList:Array<Dynamic> = Reflect.field(_dataObj, "SoundBoard");
 
 				_tabArea.width = stage.stageWidth;
 				_tabArea.height = stage.stageHeight;
 				_buttonHeight = _tabArea.tabButtonHeight;
 
 				// Setup Scroll Panel Area for button
-				_scrollPanel = new ScrollPane({"width":200,"height":stage.stageHeight - _buttonHeight + 6} );
+				_scrollPanel = new ScrollPane({"width": 200, "height": stage.stageHeight - _buttonHeight + 6});
 				_scrollPanel.backgroundColor = 0;
 
 				// Build buttons for tab
-				for(i in 0 ... soundBoardList.length)
-				{
-					
+				for (i in 0...soundBoardList.length) {
 					var buttonHolder:VerticalContainer = new VerticalContainer();
 					buttonHolder.align = "left";
 					buttonHolder.width = _tabArea.width;
 					buttonHolder.height = stage.stageHeight - _buttonHeight;
-					//buttonHolder.drawOnResize = true;
 
-					buttonHolder.name = Reflect.field(soundBoardList[i],"BoardName") + "_buttonHolder";
+
+					buttonHolder.name = Reflect.field(soundBoardList[i], "BoardName") + "_buttonHolder";
 
 					// Make sure button list is there object
-					if(Reflect.hasField(soundBoardList[i],"Button"))
-					{
-						
-						var buttonList:Array<Dynamic> = Reflect.field(soundBoardList[i],"Button");
+					if (Reflect.hasField(soundBoardList[i], "Button")) {
+						var buttonList:Array<Dynamic> = Reflect.field(soundBoardList[i], "Button");
+
+						trace(Reflect.field(soundBoardList[i],"TextColor"));
 
 						// Start putting together menu data for buttons
-						for( j in 0 ... buttonList.length)
-						{
+						for (j in 0...buttonList.length) {
 
+							var textColor : Int = (Reflect.hasField(buttonList[j],"TextColor")) ?  Std.parseInt(Reflect.field(buttonList[j],"TextColor")) : defaultTextColor;
+							var buttonColor : Int = (Reflect.hasField(buttonList[j],"ButtonColor")) ?  Std.parseInt(Reflect.field(buttonList[j],"ButtonColor")) : defaultButtonColor;
+							
 							// Switch over to Sound Button Class
-							var button:SoundButton = new SoundButton({"Label":{"textColor":0x000000,"size":BUTTON_FONT_SIZE},"text": Reflect.field(buttonList[j],"Name"),"width":200, "height": 80} );
-							var soundDataList:Array<Dynamic> = Reflect.field(buttonList[j],"Sound");
+							var button:SoundButton = new SoundButton({
+								"Label": {"textColor": textColor, "size": BUTTON_FONT_SIZE},
+								"defaultColor":buttonColor,
+								"text": Reflect.field(buttonList[j], "Name"),
+								"width": 200,
+								"height": 80
+							});
+							var soundDataList:Array<Dynamic> = Reflect.field(buttonList[j], "Sound");
+							
 
 							button.soundDataList = soundDataList;
 
-
-							for( a in 0 ... soundDataList.length)
-							{
-								#if !html5 
-									_soundManager.load( Reflect.field(soundDataList[a],"Sound"), Std.string(FileSystem.absolutePath("") + "/sound/" + Reflect.field(soundDataList[a],"Sound") + ".ogg") );
-									trace("Sound : " + Reflect.field(soundDataList[a],"Sound") + " => " + FileSystem.exists(FileSystem.absolutePath("") + "/sound/" + Reflect.field(soundDataList[a],"Sound") + ".ogg" ));
+							for (a in 0...soundDataList.length) {
+								#if !html5
+								_soundManager.load(Reflect.field(soundDataList[a], "Sound"),
+									Std.string(FileSystem.absolutePath("") + "/sound/" + Reflect.field(soundDataList[a], "Sound") + ".ogg"));
+								trace("Sound : "
+									+ Reflect.field(soundDataList[a], "Sound")
+									+ " => "
+									+ FileSystem.exists(FileSystem.absolutePath("") + "/sound/" + Reflect.field(soundDataList[a], "Sound") + ".ogg"));
 								#end
-								
 							}
-							
-							
 
-							//button.loop = (Reflect.hasField(buttonList[j],"loop")) ? Reflect.field(buttonList[j],"loop") : false;
-							button.addEventListener(MouseEvent.CLICK,onSoundButtonClick,false,0,true);
+							button.addEventListener(MouseEvent.CLICK, onSoundButtonClick, false, 0, true);
 							buttonHolder.addElement(button);
-
-							
 						}
 
-						_tabArea.addItem(Reflect.field(soundBoardList[i],"BoardName"),buttonHolder);
-						//_tabArea.getTabButton(i).buttonMode = true;
-						_tabArea.getTabButton(i).addEventListener(MouseEvent.CLICK, onTabButtonClick,false,0,true);
-					}
+						_tabArea.addItem(Reflect.field(soundBoardList[i], "BoardName"), buttonHolder);
+						_tabArea.getTabButton(i).addEventListener(MouseEvent.CLICK, onTabButtonClick, false, 0, true);
 
+						 if(Reflect.hasField(soundBoardList[i],"TextColor"))
+						 	_tabArea.getTabButton(i).textColor = Std.parseInt(Reflect.field(soundBoardList[i],"TextColor"));
+
+						 if(Reflect.hasField(soundBoardList[i],"ButtonColor"))
+						 	_tabArea.getTabButton(i).defaultColor = Std.parseInt(Reflect.field(soundBoardList[i],"ButtonColor"));
+						
+						_tabArea.getTabButton(i).draw();
+
+					}
 				}
 
 				addChild(_tabArea);
-
-			}
-			else 
-			{
+			} else {
 				trace("SoundBoard Object not found");
 			}
-		}
-		else 
-		{
-			//TODO: Create data file if one now found
+		} else {
+			// TODO: Create data file if one now found
 			trace("Unabled to find file");
 		}
-
-
-
 	}
 
-	private  function onTabButtonClick(event:MouseEvent) :Void
-	{
-		if(_scrollPanel.parent != null)
+	private function onTabButtonClick(event:MouseEvent):Void {
+		if (_scrollPanel.parent != null)
 			removeChild(_scrollPanel);
-		
 	}
 
-	private function onSoundButtonClick(event:MouseEvent) :Void
-	{
-		_currentButton = cast(event.currentTarget,SoundButton);
-		
+	private function onSoundButtonClick(event:MouseEvent):Void {
+		_currentButton = cast(event.currentTarget, SoundButton);
+
 		var buttonSoundInfoList:Array<Dynamic> = _currentButton.soundDataList;
 
-		var subButtonHolder:BaseContainer = new BaseContainer({"width":200,"height":(stage.stageHeight - (_buttonHeight + 6)),"background":false});
+		var subButtonHolder:BaseContainer = new BaseContainer({"width": 200, "height": (stage.stageHeight - (_buttonHeight + 6)), "background": false});
 
+		for (i in 0...buttonSoundInfoList.length) {
 
-		for(i in 0 ... buttonSoundInfoList.length)
-		{
-			var button:SoundButton = new SoundButton( {"Label":{"textColor":0x000000,"size":BUTTON_FONT_SIZE},"name":"button_" + i, "Loop": Reflect.field(buttonSoundInfoList[i],"Loop"), "SoundName": Reflect.field(buttonSoundInfoList[i],"Sound"), "Music":Reflect.hasField(buttonSoundInfoList[i],"Music"),"text":Reflect.field(buttonSoundInfoList[i],"Name"),"width": 200, "height": 80 });
+			var textColor : Int = (Reflect.hasField(buttonSoundInfoList[i],"TextColor")) ?  Std.parseInt(Reflect.field(buttonSoundInfoList[i],"TextColor")) : defaultTextColor;
+			var buttonColor : Int = (Reflect.hasField(buttonSoundInfoList[i],"ButtonColor")) ?  Std.parseInt(Reflect.field(buttonSoundInfoList[i],"ButtonColor")) : defaultButtonColor;
+
+			var button:SoundButton = new SoundButton({
+				"Label": {"textColor": textColor, "size": BUTTON_FONT_SIZE},
+				"name": "button_" + i,
+				"defaultColor":buttonColor,
+				"Loop": Reflect.field(buttonSoundInfoList[i], "Loop"),
+				"SoundName": Reflect.field(buttonSoundInfoList[i], "Sound"),
+				"Music": Reflect.hasField(buttonSoundInfoList[i], "Music"),
+				"text": Reflect.field(buttonSoundInfoList[i], "Name"),
+				"width": 200,
+				"height": 80
+			});
 
 			button.addEventListener(MouseEvent.CLICK, onPlaySoundbuttonClick, false, 0, true);
 
 			button.x = 0;
 			button.y = button.height * i;
 
-			cast(subButtonHolder.content,Sprite).addChild(button);
+			cast(subButtonHolder.content, Sprite).addChild(button);
 		}
 
 		_scrollPanel.x = _currentButton.x + _scrollPanel.width;
@@ -245,28 +237,18 @@ class Main extends Sprite
 		_scrollPanel.width = 200;
 		_scrollPanel.height = subButtonHolder.height;
 
-
 		subButtonHolder.draw();
 		_scrollPanel.refreshPane();
-
 
 		_scrollPanel.draw();
 
 		addChild(_scrollPanel);
-
 	}
 
-	private function onPlaySoundbuttonClick(event:MouseEvent) : Void
-	{
-		
-		var soundButton:SoundButton = cast(event.currentTarget,SoundButton);
+	private function onPlaySoundbuttonClick(event:MouseEvent):Void {
+		var soundButton:SoundButton = cast(event.currentTarget, SoundButton);
 
 		// Come up with way to cache sound files after being loaded.
 		_soundManager.playSound(soundButton.soundName);
-
 	}
-
-
-
-
 }
---
+++ src/SoundTabPane.hx
@@ -0,0 +1,123 @@
+import openfl.events.Event;
+import openfl.events.MouseEvent;
+import com.chaos.ui.classInterface.IButton;
+import openfl.display.DisplayObject;
+import com.chaos.ui.UIStyleManager;
+import com.chaos.ui.TabPane;
+import com.chaos.ui.Button;
+
+class SoundTabPane extends TabPane 
+{
+    public function new(data:Dynamic = null)
+    {
+        super(data);
+    }
+
+    override function addItem(value:String, content:DisplayObject):Dynamic {
+        
+        var obJ:Dynamic = super.addItem(value, content);
+        var button:IButton = getTabButton(buttonArea.numChildren - 1);
+
+        button.textColor = _tabButtonTextColor;
+        button.defaultColor = _tabButtonNormalColor;
+        button.overColor = _tabButtonOverColor;
+        button.downColor = _tabButtonSelectedColor;
+        button.disableColor = _tabButtonDisableColor;
+
+        return obJ;
+
+    }
+
+    override function draw() {
+
+
+		if (null == _contentList)
+			return;
+
+		//super.draw();
+
+		// Create and resize buttons
+		for (i in 0 ... buttonArea.numChildren)
+		{
+
+			// Setting up buttons
+            var button:Button = cast(buttonArea.getChildAt(i), Button);
+			
+			// Update all the i
+			_contentList.getItemAt(i).id = i;
+			
+			button.name = Std.string(i);
+			button.width = width / buttonArea.numChildren;
+			button.height = _tabButtonHeight;
+			button.x = button.width * i;
+			button.y = 0;
+
+			// Set TextFormat based on UIStyleManager
+			button.textBold = UIStyleManager.TABPANE_BUTTON_TEXT_BOLD;
+			button.textItalic = UIStyleManager.TABPANE_BUTTON_TEXT_ITALIC;
+
+			if ( -1 != UIStyleManager.TABPANE_BUTTON_TEXT_SIZE)
+				button.textSize = UIStyleManager.TABPANE_BUTTON_TEXT_SIZE;
+
+			if ("" != UIStyleManager.TABPANE_BUTTON_TEXT_FONT)
+				button.textFont = UIStyleManager.TABPANE_BUTTON_TEXT_FONT;
+
+			if (null != UIStyleManager.TABPANE_BUTTON_TEXT_EMBED)
+				button.label.setEmbedFont(UIStyleManager.TABPANE_BUTTON_TEXT_EMBED);
+
+			if ( -1 != UIStyleManager.TABPANE_BUTTON_TINT_ALPHA)
+				button.label.borderAlpha = UIStyleManager.TABPANE_BUTTON_TINT_ALPHA;
+				
+			button.draw();
+
+		}
+
+		// Set location of scroll pane
+		buttonArea.x = 0;
+		buttonArea.y = 0;
+		
+		_scrollPane.width = _width;
+		_scrollPane.height = _height - _tabButtonHeight;
+		_scrollPane.y = _tabButtonHeight;
+
+		// Load in content
+		if (_contentList.length > 0 && _contentList.getItemAt(_selectedIndex).content != null)
+		{
+			cast(buttonArea.getChildByName(Std.string(_selectedIndex)), Button).enabled = false;
+			contentLoad(_contentList.getItemAt(_selectedIndex).content);
+		}
+
+		_scrollPane.update();
+		_scrollPane.draw();
+
+    }
+
+    override function tabPress(event:MouseEvent) {
+        //super.tabPress(event);
+
+		var button:Button =  cast(event.currentTarget, Button);
+		var oldButton:Button = cast(buttonArea.getChildByName(Std.string(_selectedIndex)), Button);
+
+		if (_selectedIndex != Std.parseInt(button.name))
+		{
+			// Current Button
+			button.enabled = false;
+
+			// Disable old one
+			oldButton.enabled = true;
+			
+			button.draw();
+			oldButton.draw();
+
+			// Update selected index and grab new content
+			_selectedIndex = Std.parseInt(button.name);
+			contentLoad(cast(_contentList.getItemAt(Std.parseInt(button.name)).content, DisplayObject));
+
+			dispatchEvent(new Event(Event.CHANGE));
+			
+		}        
+
+
+    }
+
+}
